c1b03ce20e856dcd2c707037274fc2e54a9ed896
Yavisht Katgara <yavisht@users.noreply.github.com>
2017-09-12 14:16:27 +1000

    Remove Posts and comments

--- hide-menu-items.php
+++ hide-menu-items.php
@@ -1,6 +1,6 @@
 <?php
 ///////////////////////////////////////////////////////
-///// Disable Posts and Comments from Dashboard
+///// E.g. Disable Posts and Comments from Dashboard
 ///////////////////////////////////////////////////////
 
 // Uncomment the lines that you wish to hide
@@ -9,10 +9,10 @@ function yk_custom_remove_menus(){
 
 	//remove_menu_page( 'index.php' );                  //Dashboard
 	//remove_menu_page( 'jetpack' );                    //Jetpack
-	//remove_menu_page( 'edit.php' );                   //Posts
+	remove_menu_page( 'edit.php' );                   //Posts
 	//remove_menu_page( 'upload.php' );                 //Media
 	//remove_menu_page( 'edit.php?post_type=page' );    //Pages
-	//remove_menu_page( 'edit-comments.php' );          //Comments
+	remove_menu_page( 'edit-comments.php' );          //Comments
 	//remove_menu_page( 'themes.php' );                 //Appearance
 	//remove_menu_page( 'plugins.php' );                //Plugins
 	//remove_menu_page( 'users.php' );                  //Users
